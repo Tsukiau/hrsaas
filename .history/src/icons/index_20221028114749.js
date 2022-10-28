238a3dcc1ceb99fe80e4414f10f7e5ba2059d967
tsuki <435746115@qq.com>
2022-10-28 13:52:45 +0800

    中台

---
+++ .history/src/icons/index_20221028114749.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+
+// 把所有的.svg 都引入到项目中
---
+++ .history/src/store/modules/user_20221028113246.js
@@ -0,0 +1,10 @@
+export default {
+  name
+   state: {},
+   mutations: {
+    
+   },
+   actions: {
+    
+   }
+}
